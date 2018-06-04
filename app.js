c86ca601ae674fb3c652983d045a068f31c4cd0a
RedondaAnalia <a.redonda89@gmail.com>
2018-06-04 20:21:12 -0300

    Fin de la seccion 10

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // Requires
 var express= require ('express');
 var mongoose= require('mongoose');
+var bodyParser= require ('body-parser');
 
 
 // Inicializar variables
 var app = express();
 
-// Conexio a la BBDD
+
+// Body Parser
+//parse application/ x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
+//parse application/json
+app.use(bodyParser.json());
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes= require ('./routes/usuario');
+var loginRoutes= require ('./routes/login');
+
+
+// Conexion a la BBDD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     
     if ( err ) throw err;
@@ -16,16 +31,13 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 
 // Rutas
-app.get('/', (req, res, next ) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-})
+
 
 // Escuchar peticiones
 app.listen(3000, () => {
     console.log('Express Server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
-    
 });
